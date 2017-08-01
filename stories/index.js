00ce9e510ec2e23a56887640e6358ce5d2192eb1
Zhenyu Lin <zhenyu.lyn@gmail.com>
2017-08-01 12:51:08 +0100

    add storybook

---
+++ stories/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import { ChartExample } from '../src';
+
+storiesOf('ChartExample', module).add('standard', () => <ChartExample />);
