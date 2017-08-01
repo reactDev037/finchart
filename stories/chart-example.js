6b71aaf4570ab2d27b239296e0ec9920e8ec743c
Zhenyu Lin <zhenyu.lyn@gmail.com>
2017-08-01 16:56:33 +0100

    finchart-example test

--- stories/chart-example.js
+++ stories/chart-example.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 
-import { ChartExample } from '../src';
+import { ChartExample } from 'finchart-example';
 
 storiesOf('ChartExample', module).add('standard', () => <ChartExample />);
