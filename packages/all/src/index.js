c24d8dd44380eba675af59f9a62a612a3322e08e
Zhenyu Lin <zhenyu.lyn@gmail.com>
2017-08-01 16:49:09 +0100

    config example and all packages

---
+++ packages/all/src/index.js
@@ -0,0 +1,4 @@
+export * from 'finchart-axis';
+export * from 'finchart-shape';
+export * from 'finchart-chart';
+export * from 'finchart-util';
---
+++ packages/example/src/index.js
@@ -0,0 +1,1 @@
+export ChartExample from './ChartExample';
